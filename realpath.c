b69cfe4c170381e7e01daed425f394fa3fb13501
Michael C. Browne <mcbrowne@slac.stanford.edu>
2013-08-06 21:05:53 +0000

    This shouldn't seg fault.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/release/macro@13589 ececa60f-9f63-0410-97c7-ea05888eb260

--- realpath.c
+++ realpath.c
@@ -4,8 +4,11 @@
 
 int main(int argc, char **argv)
 {
+    char *s = NULL;
     if (argc > 1)
-        printf("%s\n", realpath(argv[1], NULL));
+        s = realpath(argv[1], NULL);
+    if (s)
+        printf("%s\n", s);
     else
         printf("\n");
     return 0;
